879107ed197d3ed854b21ed9442c3b2d84a9d718
axtezy <axtezy@gmail.com>
2024-02-22 08:45:20 +0100

    Update snap version

--- packages/snap/src/images/bad-rate.ts
+++ packages/snap/src/images/bad-rate.ts
@@ -1,5 +1,5 @@
 export const badRate = `<svg width="500" height="250" viewBox="0 0 500 250" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<rect width="500" height="250" rx="25" fill="url(#pattern0)"/>
+<rect width="500" height="250" rx="50" fill="url(#pattern0)"/>
 <defs>
 <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
 <use xlink:href="#image0_1647_159" transform="scale(0.0005 0.001)"/>
